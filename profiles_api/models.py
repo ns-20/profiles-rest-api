68886372e3606344dbf6623f0e38914e4a99fafa
NS-20 <njorogesammy@yahoo.com>
2022-01-06 21:12:52 +0000

    added user model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstactBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in teh system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=Fales)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name'] 
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retriev short name of user"""
+        return self.name
+
+    def __str__(self):
+        """Retrieve string representation of the user"""
+        return self.email
+
